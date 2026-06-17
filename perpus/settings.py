d61eb7d66e492d084c8bb5c182e0701f4f9b3921
Ghozy Aslam <masghozyaslam@gmail.com>
2026-06-17 13:45:16 +0700

    Update Django

--- perpus/settings.py
+++ perpus/settings.py
@@ -7,11 +7,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-z%f9q0e+e(8p@&cc#(8iqaw!4)64jb_+bc7mk5&jk_er8*#d=!')
 
-# Di Vercel sebaiknya True dulu saat uji coba, jika sudah lancar bisa diubah ke False
-DEBUG = False
-
-# Wajib agar domain dari Vercel bisa mengakses Django kamu
-ALLOWED_HOSTS = ['GhozyAslam.pythonanywhere.com']
+if 'PYTHONANYWHERE_SITE' in os.environ:
+    ALLOWED_HOSTS = ['GhozyAslam.pythonanywhere.com']
+    DEBUG = False 
+else:
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    DEBUG = True  
 
 
 # Application definition
@@ -106,6 +107,9 @@ STATIC_URL = 'static/'
 
 # Pengaturan lokasi static files khusus untuk Vercel Serverless
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = []
+_local_static = os.path.join(BASE_DIR, 'static')
+if os.path.exists(_local_static):
+    STATICFILES_DIRS.append(_local_static)
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
