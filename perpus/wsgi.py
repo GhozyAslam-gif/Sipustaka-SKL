f8b59b23c988883ea4adc9542087778b75ad4696
Ghozy Aslam <masghozyaslam@gmail.com>
2026-06-17 19:58:46 +0700

    Update Django

--- perpus/wsgi.py
+++ perpus/wsgi.py
@@ -1,14 +1,13 @@
 import os
 import sys
 
-
-path = '/home/GhozyAslam/perpus'  
+# Jalur direktori utama tempat proyek Anda berada
+path = '/home/GhozyAslam/Sipustaka-SKL/perpus'
 if path not in sys.path:
     sys.path.insert(0, path)
 
-
+# Mengarahkan ke file settings.py di dalam folder perpus Anda
 os.environ['DJANGO_SETTINGS_MODULE'] = 'perpus.settings'
 
-# 3. Jalankan aplikasi WSGI Django
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
